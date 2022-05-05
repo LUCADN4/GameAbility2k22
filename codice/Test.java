b90b6e6086973d0c9ee1016a33ef098a7e72ffa1
galie10 <90908029+galie10@users.noreply.github.com>
2022-05-05 16:48:02 +0200

    Update Test.java

--- codice/Test.java
+++ codice/Test.java
@@ -35,7 +35,7 @@ public class Test extends PApplet {
     int posizione; //define an integer variable, it's used as an index like "indice"
     private Button btn; //is an object of Bottone called "btn"
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         PApplet.main("Test");
     } //we define the main of the test using PApplet library
 
@@ -47,12 +47,12 @@ public class Test extends PApplet {
         posizioniX = new int[]{300, 450, 600, 750, 900, 1050, 1200}; //vector that contains the position to draw every piece (horizontal)
         posizioniY = new int[]{150, 250, 350, 450, 550, 650}; //vector that contains the position to draw every piece (vertical)
         s = new SchermataIniziale(this);
-        int boundRate = 115200; //we use this boundRate to communicate with the software Arduino,
+        //int boundRate = 115200; //we use this boundRate to communicate with the software Arduino,
         // 115200 represent the amount of data we can pass throught the softwares
-        this.arduino = new Arduino("COM5", boundRate); //we initialize the standard port which connect the arudino physically
-        this.arduino.openConnection(); //we open the connection of the arduino using the instance of the class "Arduino"
-        boolean connected = arduino.openConnection(); //we define a boolean variable to know if the arduino can communicate with java or not
-        System.out.println("connection established: " + connected); //we print on the screen the result of the connection
+        //this.arduino = new Arduino("COM5", boundRate); //we initialize the standard port which connect the arudino physically
+        //this.arduino.openConnection(); //we open the connection of the arduino using the instance of the class "Arduino"
+        //boolean connected = arduino.openConnection(); //we define a boolean variable to know if the arduino can communicate with java or not
+        //System.out.println("connection established: " + connected); //we print on the screen the result of the connection
         photo = loadImage("images.jpeg"); //we assign the images with their path to a variable
         fine1 = loadImage("fp1vers2.png");//we assign the images with their path to a variable
         fine2 = loadImage("fp2vers2.jpeg");//we assign the images with their path to a variable
@@ -73,16 +73,15 @@ public class Test extends PApplet {
         buttonClicked = true; //initialize the variable to true
         InputTastiera t = new InputTastiera(); //enable the flow of the keyboard keys through the class "InputTastiera"
         c = new Campo(); //create the game field
-        pedinaDinamica = new Indicatore(this, 225,175,275,90,50,50, Costanti.ROSSO, Color.black);
+        pedinaDinamica = new Indicatore(this, 225, 175, 275, 90, 50, 50, Costanti.ROSSO, Color.black);
         //is the indicator that slides horizontally from left to right
         giocatore1 = "giocatore1"; //assign a default name to giocatore 1
         giocatore2 = "giocatore2"; //assign a default name to giocatore 2
         keys = new boolean[128]; //assing the space for the boolean vector of the keyboard
         posizione = 0; //initialize the position to 0
-
     }
 
-    public void draw() { //method that shows the game
+        public void draw() { //method that shows the game
         switch (mode) { //switch case for the different slides of the game
             case 1 -> { //start page case
                 background(0, 0, 0); //set the background to black
@@ -108,17 +107,17 @@ public class Test extends PApplet {
             case 3 -> {
                 delay(2500); //we set a delay of 2.5 seconds to show the player won and then the victory slide
                 image(fine2, 0,0); //image of the victory of the second player
-                this.arduino.closeConnection(); //close the connection with the arduino
+                //this.arduino.closeConnection(); //close the connection with the arduino
             }
             case 4 -> {
                 delay(2500); //we set a delay of 2.5 seconds to show the player won and then the victory slide
                 image(fine1, 0,0); //image of the victory of the first player
-                this.arduino.closeConnection(); //close the connection with the arduino
+                //this.arduino.closeConnection(); //close the connection with the arduino
             }
             case 5 -> {
                 delay(2500); //we set a delay of 2.5 seconds to show the player won and then the draw slide
                 image(pareggio, 0,0); //image of the draw between the two players
-                this.arduino.closeConnection(); //close the connection with the arduino
+                //this.arduino.closeConnection(); //close the connection with the arduino
             }
         }
     }
@@ -164,9 +163,9 @@ public class Test extends PApplet {
         }
     }
 
-    public void keyPressed() { //method to enable keyboard keys
-
-        if(key == 'f' || key == 'F'){ //shows if the "f" key is pressed
+    public void keyPressed() { //method to enable keyboard keysc
+        keyRepeatEnabled = false;
+        if(key == ' ' || key == 'F'){ //shows if the "f" key is pressed
             if(pedinaDinamica.getX1() >= 225 && pedinaDinamica.getX1() < 375) posizione = 0; //if the piece's indicator is between 225 and 375 of x coordinates,
                 // then the piece is going to be put inside the first column
             else if(pedinaDinamica.getX1() >= 375 && pedinaDinamica.getX1() < 525) posizione = 1;//if the piece's indicator is between 375 and 525 of x coordinates,
@@ -181,10 +180,11 @@ public class Test extends PApplet {
                 // then the piece is going to be put inside the sixth column
             else if(pedinaDinamica.getX1() >= 1125 && pedinaDinamica.getX1() < 1275) posizione = 6;//if the piece's indicator is between 1125 and 1275 of x coordinates,
             // then the piece is going to be put inside the seventh column
-            arduino.serialWrite("1"); //used to set the arduino pin of the hand to HIGH so it do the predefinite movement
+            //arduino.serialWrite("1"); //used to set the arduino pin of the hand to HIGH so it do the predefinite movement
         }
 
-        if(pedineInserite != 42 && !c.vittoriaColonna() && !c.vittoriaRiga() && !c.vittoriaDiagonale()) {//checks vertical, horizontal and diagonal victories
+        if(pedineInserite != 42 && !c.vittoriaColonna() && !c.vittoriaRiga() && !c.vittoriaDiagonale()) {
+            //checks vertical, horizontal and diagonal victories
             if (pedineInserite % 2 == 0) {
 
                 c.inserisciPedina(TipoColore.ROSSO, giocatore1, posizione);
